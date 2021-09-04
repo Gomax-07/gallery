75c85da268a1b8bb8e777d79cf495a7f193d9412
go-max07 <johnmetry17@gmail.com>
2021-09-04 12:12:30 +0300

    Installed db driver and created models file

--- gallery1/settings.py
+++ gallery1/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'gallery1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
--- pic/models.py
+++ pic/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    pass
+
+
+class Creator:
+    pass
