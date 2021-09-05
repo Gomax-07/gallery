5b549f9262153a6cd6434328a2dc38930e7a1588
go-max07 <johnmetry17@gmail.com>
2021-09-06 02:35:13 +0300

    Created new app and form and url files

--- gallery1/settings.py
+++ gallery1/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'projects.apps.ProjectsConfig',
+    'blog.apps.BlogConfig',
     'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
