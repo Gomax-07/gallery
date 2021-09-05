aa9fff4e47562f074302a698fc22f939287f15fc
go-max07 <johnmetry17@gmail.com>
2021-09-06 02:29:23 +0300

    Changed urlconfs in project file to accomodate new app

--- gallery1/settings.py
+++ gallery1/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pic.apps.PicConfig',
+    'projects.apps.ProjectsConfig',
     'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
