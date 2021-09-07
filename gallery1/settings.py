934b667d79d9a98e43648864a420cc559e9456e6
Gomax-07 <62060943+Gomax-07@users.noreply.github.com>
2021-09-07 10:52:07 +0300

    Update of   whitenoise at Middleware

--- gallery1/settings.py
+++ gallery1/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-
 INSTALLED_APPS = [
     'projects.apps.ProjectsConfig',
     'bootstrap4',
